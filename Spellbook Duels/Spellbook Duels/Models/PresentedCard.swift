fc64ed9c71d32bdf8470f7c1ff46d1065dbd2a1f
Ryan Camp <141803241+TheSimicSlaw@users.noreply.github.com>
2025-10-27 21:54:55 -0500

    Update w/ card model

---
+++ Spellbook Duels/Spellbook Duels/Models/PresentedCard.swift
@@ -0,0 +1,28 @@
+//
+//  PresentedCard.swift
+//  Spellbook Duels
+//
+//  Created by Ryan Camp on 10/27/25.
+//
+
+import Foundation
+import UIKit
+
+enum Element {
+    case FIRE, WATER, EARTH, AIR
+}
+
+struct PresentedCard {
+    let name: String
+    let text: String
+    let type: String
+    let cost: String
+    let costVal: Int
+    let element: Element
+    let image: UIImage
+    let cardCode: String
+    let Strength: Int?
+    let duration: Int?
+    let hasActivated: Bool
+    let numTargets: Int?
+}
