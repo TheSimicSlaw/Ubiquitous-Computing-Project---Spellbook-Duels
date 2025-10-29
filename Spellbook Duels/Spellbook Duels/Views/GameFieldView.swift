952a7425f9adda7f7dee2b2052f51f7c8c391653
Rocky Williams <rleonard948@gmail.com>
2025-10-29 12:40:47 -0500

    Initial GameFieldView

--- Spellbook Duels/Spellbook Duels/Views/GameFieldView.swift
+++ Spellbook Duels/Spellbook Duels/Views/GameFieldView.swift
@@ -9,8 +9,110 @@ import SwiftUI
 
 struct GameFieldView: View {
     var body: some View {
-        Color("AccentOne")
-            .ignoresSafeArea()
+        ZStack {
+            FieldBackgroundView()
+            VStack {
+                OpponentFieldView()
+                Spacer()
+                Divider()
+                    .frame(height: 2)
+                    .overlay(Color.black)
+                //Spacer() <- Doesn't work as expected because the width exceeds the screen
+                PlayerFieldView()
+            }
+            HStack {
+                Rectangle() // Discard Pile
+                    .fill(.black.opacity(0.5))
+                    .frame(width: 50, height: 100)
+                    .padding(.trailing, 150)
+                Rectangle() // Combined Spellbook
+                    .fill(Color.accentOne)
+                    .frame(width: 60, height: 100)
+                    .padding(.leading, 150)
+            }
+        }
+    }
+}
+
+struct FieldBackgroundView: View {
+    var body: some View {
+        Image("gamefieldbg")
+            .resizable()
+            .scaledToFill()
+            .rotationEffect(Angle(degrees: 90))
+            .ignoresSafeArea(edges: .horizontal)
+    }
+}
+
+struct OpponentFieldView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "book")
+                .font(.system(size: 100))
+                .fontWeight(.thin)
+                .foregroundStyle(.white)
+            
+            HStack(spacing: 24) {
+                Rectangle()
+                    .fill(.gray)
+                    .frame(width: 60, height: 84)
+                Rectangle()
+                    .fill(.gray)
+                    .frame(width: 60, height: 84)
+                Rectangle()
+                    .fill(.gray)
+                    .frame(width: 60, height: 84)
+            }
+            
+            HStack(spacing: 19) {
+                Rectangle()
+                    .fill(.gray)
+                    .frame(width: 60, height: 84)
+                Rectangle()
+                    .fill(.gray)
+                    .frame(width: 80, height: 112)
+                    .padding(.top, 11)
+                Rectangle()
+                    .fill(.gray)
+                    .frame(width: 60, height: 84)
+            }
+        }
+        
+    }
+}
+
+struct PlayerFieldView: View {
+    var body: some View {
+        VStack {
+            HStack(spacing: 19) {
+                Rectangle()
+                    .fill(.gray)
+                    .frame(width: 60, height: 84)
+                Rectangle()
+                    .fill(.gray)
+                    .frame(width: 80, height: 112)
+                    .padding(.top, 11)
+                Rectangle()
+                    .fill(.gray)
+                    .frame(width: 60, height: 84)
+            }
+            
+            HStack(spacing: 24) {
+                Rectangle()
+                    .fill(.gray)
+                    .frame(width: 60, height: 84)
+                Rectangle()
+                    .fill(.gray)
+                    .frame(width: 60, height: 84)
+                Rectangle()
+                    .fill(.gray)
+                    .frame(width: 60, height: 84)
+            }
+            Image(systemName: "book")
+                .font(.system(size: 100))
+                .fontWeight(.thin)
+                .foregroundStyle(.white)
+        }
     }
 }
 
