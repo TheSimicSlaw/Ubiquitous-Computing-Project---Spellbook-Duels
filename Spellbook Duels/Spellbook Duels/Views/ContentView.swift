d163aa1ef247f88b2e5a3a08af51e52070926537
Ryan Camp <141803241+TheSimicSlaw@users.noreply.github.com>
2025-10-30 16:08:39 -0500

    Revert "ContentView Update to enable base set of cards"
    This reverts commit f83ff46b1f3026d66b31b9596135d8aabfb773df.

--- Spellbook Duels/Spellbook Duels/Views/ContentView.swift
+++ Spellbook Duels/Spellbook Duels/Views/ContentView.swift
@@ -27,15 +27,15 @@ struct ContentView: View {
                         Label("Library", systemImage: "book")
                     }
             }
+//            .task {
+//                if decks.isEmpty {
+//                    modelContext.insert(DeckListModel(deckname: "Counterburn", cardList: ["WIN", "FAS", "FSP", "FGR", "FPU", "WSL", "WSC", "WRE", "FBL", "FFI", "FIR", "WHY", "FFL", "FWA", "FOU"], cardCounts: ["WIN": 4, "FAS": 4, "FSP": 4, "FGR": 4, "FPU": 4, "WSL": 4, "WSC": 4, "WRE": 4, "FBL": 4, "Fire Wand": 4, "FIR": 4, "WHY": 4, "FFL": 4, "FWA": 4, "FOU": 4], deckElements: [Element.FIRE, Element.WATER]))
+//                    modelContext.insert(DeckListModel(deckname: "Counterburn 2", cardList: ["WIN", "FAS", "FSP", "FGR", "FPU", "WSL", "WSC", "WRE", "FBL", "FFI", "FIR", "WHY", "FFL", "FWA", "FOU"], cardCounts: ["WIN": 4, "FAS": 4, "FSP": 4, "FGR": 4, "FPU": 4, "WSL": 4, "WSC": 4, "WRE": 4, "FBL": 4, "Fire Wand": 4, "FIR": 4, "WHY": 4, "FFL": 4, "FWA": 4, "FOU": 4], deckElements: [Element.FIRE, Element.WATER]))
+//                    modelContext.insert(DeckListModel(deckname: "Earth/Air", cardList: ["WIN", "FAS", "FSP", "FGR", "FPU", "WSL", "WSC", "WRE", "FBL", "FFI", "FIR", "WHY", "FFL", "FWA", "FOU"], cardCounts: ["WIN": 4, "FAS": 4, "FSP": 4, "FGR": 4, "FPU": 4, "WSL": 4, "WSC": 4, "WRE": 4, "FBL": 4, "Fire Wand": 4, "FIR": 4, "WHY": 4, "FFL": 4, "FWA": 4, "FOU": 4], deckElements: [Element.AIR, Element.EARTH]))
+//                    try? modelContext.save()
+//                }
+//            }
         }
-       .task {
-           if decks.isEmpty {
-               modelContext.insert(DeckListModel(deckname: "Counterburn", cardList: ["WIN", "FAS", "FSP", "FGR", "FPU", "WSL", "WSC", "WRE", "FBL", "FFI", "FIR", "WHY", "FFL", "FWA", "FOU"], cardCounts: ["WIN": 4, "FAS": 4, "FSP": 4, "FGR": 4, "FPU": 4, "WSL": 4, "WSC": 4, "WRE": 4, "FBL": 4, "Fire Wand": 4, "FIR": 4, "WHY": 4, "FFL": 4, "FWA": 4, "FOU": 4], deckElements: [Element.FIRE, Element.WATER]))
-               modelContext.insert(DeckListModel(deckname: "Counterburn 2", cardList: ["WIN", "FAS", "FSP", "FGR", "FPU", "WSL", "WSC", "WRE", "FBL", "FFI", "FIR", "WHY", "FFL", "FWA", "FOU"], cardCounts: ["WIN": 4, "FAS": 4, "FSP": 4, "FGR": 4, "FPU": 4, "WSL": 4, "WSC": 4, "WRE": 4, "FBL": 4, "Fire Wand": 4, "FIR": 4, "WHY": 4, "FFL": 4, "FWA": 4, "FOU": 4], deckElements: [Element.FIRE, Element.WATER]))
-               modelContext.insert(DeckListModel(deckname: "Earth/Air", cardList: ["WIN", "FAS", "FSP", "FGR", "FPU", "WSL", "WSC", "WRE", "FBL", "FFI", "FIR", "WHY", "FFL", "FWA", "FOU"], cardCounts: ["WIN": 4, "FAS": 4, "FSP": 4, "FGR": 4, "FPU": 4, "WSL": 4, "WSC": 4, "WRE": 4, "FBL": 4, "Fire Wand": 4, "FIR": 4, "WHY": 4, "FFL": 4, "FWA": 4, "FOU": 4], deckElements: [Element.AIR, Element.EARTH]))
-               try? modelContext.save()
-           }
-       }
     }
 }
 
