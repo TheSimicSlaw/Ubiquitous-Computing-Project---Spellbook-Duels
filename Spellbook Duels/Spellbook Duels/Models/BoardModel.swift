4077f5a36c54caa7f457b61ec8ce7ac258126b11
Rocky Williams <rleonard948@gmail.com>
2025-11-25 14:46:03 -0600

    Added Board Model

---
+++ Spellbook Duels/Spellbook Duels/Models/BoardModel.swift
@@ -0,0 +1,22 @@
+//
+//  BoardModel.swift
+//  Spellbook Duels
+//
+//  Created by Rocky Williams on 11/25/25.
+//
+
+struct BoardModel {
+    var playerCurse: String = ""
+    var playerJinx: String = ""
+    var playerWard: String = ""
+    var playerCharm: String = ""
+    var playerRelic: String = ""
+    var playerPotion: String = ""
+    
+    var opponentCurse: String = ""
+    var opponentJinx: String = ""
+    var opponentWard: String = ""
+    var opponentCharm: String = ""
+    var opponentRelic: String = ""
+    var opponentPotion: String = ""
+}
