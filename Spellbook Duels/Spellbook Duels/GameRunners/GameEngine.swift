4580fc6a4760212b2508591d78681789872c3ba6
Ryan Camp <141803241+TheSimicSlaw@users.noreply.github.com>
2025-11-27 18:33:35 -0600

    Basic GameEngine file - just enough for StrengthModifiers to work

---
+++ Spellbook Duels/Spellbook Duels/GameRunners/GameEngine.swift
@@ -0,0 +1,16 @@
+//
+//  GameEngine.swift
+//  Spellbook Duels
+//
+//  Created by Ryan Camp on 11/26/25.
+//
+
+import Foundation
+
+final class GameEngine {
+    var board: BoardModel
+    
+    init(board: BoardModel) {
+        self.board = board
+    }
+}
