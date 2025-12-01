678dd35218ffaf4792cb13c399d91fa5e4436095
Rocky Williams <rleonard948@gmail.com>
2025-12-01 14:03:21 -0600

    added player deck view with its respective functions. added glowing outlines. added database stuff

---
+++ Spellbook Duels/Spellbook Duels/Models/PlayerDeckView.swift
@@ -0,0 +1,18 @@
+//
+//  PlayerDeckView.swift
+//  Spellbook Duels
+//
+//  Created by Rocky Williams on 12/1/25.
+//
+
+import SwiftUI
+
+struct PlayerDeckView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayerDeckView()
+}
